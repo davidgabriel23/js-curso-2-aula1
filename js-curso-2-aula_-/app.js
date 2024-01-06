51339fa8c530ccfe8ee1efa7a58868349fbb435d
davidgabriel23 <daviid.gabriel25@gmail.com>
2024-01-06 17:41:09 -0300

    function

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,10 +1,25 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
 
-let paragrafo = document.querySelector('p')
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+let numeroScreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
-    console.log('O botão foi clicado')
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroScreto);
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1 );
 }
