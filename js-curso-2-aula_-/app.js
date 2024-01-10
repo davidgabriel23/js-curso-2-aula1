8bed32943659a481400a5ecc242458c31328ebcb
davidgabriel23 <daviid.gabriel25@gmail.com>
2024-01-09 21:17:57 -0300

    Array

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -4,9 +4,11 @@
 
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroScreto = gerarNumeroAleatorio();
-let tentativas = 1
+let tentativas = 1;
+
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -18,7 +20,7 @@ function exibirMensagemInicial() {
 
 }
 
-exibirMensagemInicial()
+exibirMensagemInicial();
 
 
 function verificarChute(){
@@ -42,7 +44,21 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1 );
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1 );
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+   
 }
 
 
@@ -57,5 +73,5 @@ function reiniciarJogo(){
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled',true)
+    document.getElementById('reiniciar').setAttribute('disabled',true);
 }
